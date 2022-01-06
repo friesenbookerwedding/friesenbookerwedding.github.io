34e246c09583d66fbc9c4764a6b7e82cad564a41
ff <ff@localhost.localdomain>
2022-01-06 11:23:24 -0500

    wip reducing top menu size and animations

--- js/scripts.js
+++ js/scripts.js
@@ -93,25 +93,25 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "20px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "26px",
+                    "top": "12px",
                 });
                 $('header .navicon').css({
-                    "top": "34px",
+                    "top": "24px",
                 });
             } else {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
+                    "padding": "20px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "41px",
+                    "top": "12px",
                 });
                 $('header .navicon').css({
-                    "top": "48px",
+                    "top": "24px",
                 });
             }
         });
